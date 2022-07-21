f717094e3519b733fe95649e38dc32fe28af7b13
RigelTLT <kozyrin-di@yandex.ru>
2022-07-21 23:03:15 +0400

    feat: complete 6 tasks array

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -20,8 +20,12 @@
  *    ['Array', 'Number', 'string'], 'Date'    => -1
  *    [0, 1, 2, 3, 4, 5], 5    => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  //throw new Error('Not implemented');
+  arr.forEach((el, idx) =>{
+    if(el === value) return idx;
+  })
+  return -1;
 }
 
 /**
@@ -35,8 +39,18 @@ function findElement(/* arr, value */) {
  *    2 => [ 1, 3 ]
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  //throw new Error('Not implemented');
+  let number = Math.round(Math.random()*10);
+  let arr = [];
+  for(let i = 0; i<len;i++){
+  while(Math.round(number) % 2 == 0){
+    number = Math.round(Math.random()*10);
+  }
+  arr.push(number);
+    number = Math.round(Math.random()*10);
+  }
+  return arr;
 }
 
 
@@ -52,8 +66,11 @@ function generateOdds(/* len */) {
  *    [0, 1, 2, 3, 4, 5] => [0, 1, 2, 3, 4, 5,   0, 1, 2, 3, 4, 5]
  *    [] => []
  */
-function doubleArray(/* arr */) {
-  throw new Error('Not implemented');
+function doubleArray(arr) {
+  //throw new Error('Not implemented');
+  let result = arr;
+  arr.forEach((el)=>{result.push(el);})
+  return result;
 }
 
 
@@ -68,8 +85,16 @@ function doubleArray(/* arr */) {
  *    [-1, 2, -5, -4, 0] => [ 2 ]
  *    [] => []
  */
-function getArrayOfPositives(/* arr */) {
-  throw new Error('Not implemented');
+function getArrayOfPositives(arr) {
+  //throw new Error('Not implemented');
+  let result = [];
+  arr.forEach((item) => {
+    if(item>0){
+    result.push(item)
+  }
+}
+)
+  return result;
 }
 
 /**
@@ -83,8 +108,15 @@ function getArrayOfPositives(/* arr */) {
  *    [ 1, 2, 3, 4, 5 ] => []
  *    [ 'cat, 'dog', 'raccoon' ] => [ 'cat', 'dog', 'raccoon' ]
  */
-function getArrayOfStrings(/* arr */) {
-  throw new Error('Not implemented');
+function getArrayOfStrings(arr) {
+  //throw new Error('Not implemented');
+  let result = [];
+  arr.forEach((el) => {
+    if(typeof el === 'string'){
+      result.push(el);
+    }
+  })
+  return result;
 }
 
 /**
@@ -100,8 +132,15 @@ function getArrayOfStrings(/* arr */) {
  *    [ 1, 2, 3, 4, 5, 'false' ]         => [ 1, 2, 3, 4, 5, 'false' ]
  *    [ false, 0, NaN, '', undefined ]   => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
+function removeFalsyValues(arr) {
+  //throw new Error('Not implemented');
+  let result = [];
+  arr.forEach((el) => {
+    if(el){
+      result.push(el);
+    }
+  })
+  return result;
 }
 
 /**
